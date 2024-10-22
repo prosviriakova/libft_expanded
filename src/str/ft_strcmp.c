22104a026cdcb153cf5c0309f670d2f458a8acd9
oprosvir <ol7897@ya.ru>
2024-10-22 21:13:59 +0200

    add: ft_strcmp

---
+++ src/str/ft_strcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 21:12:22 by oprosvir          #+#    #+#             */
+/*   Updated: 2024/10/22 21:12:53 by oprosvir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
+			return (-1);
+		else if ((unsigned char)s1[i] > (unsigned char)s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
